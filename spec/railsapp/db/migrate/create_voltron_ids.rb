8f33e071184c31850f94023f6e19ed007c190ac2
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 12:24:08 -0600

    Adds migrations, installer for migrations

---
+++ spec/railsapp/db/migrate/create_voltron_ids.rb
@@ -0,0 +1,9 @@
+class CreateVoltronIds < ActiveRecord::Migration
+	def change
+		create_table :voltron_ids, id: false do |t|
+			t.integer :id, limit: 8, primary_key: true
+			t.integer :resource_id, limit: 8
+			t.string :resource_type
+		end
+	end
+end
