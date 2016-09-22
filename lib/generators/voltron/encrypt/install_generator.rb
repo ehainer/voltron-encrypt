2365b45e905bf33b23a8fe7868ec80d8784bd988
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 17:09:26 -0600

    Changed source_root for installer

--- lib/generators/voltron/encrypt/install_generator.rb
+++ lib/generators/voltron/encrypt/install_generator.rb
@@ -3,7 +3,7 @@ module Voltron
     module Generators
       class InstallGenerator < Rails::Generators::Base
 
-        source_root File.expand_path("../../../../../spec/railsapp", __FILE__)
+        source_root File.expand_path("../../../templates", __FILE__)
 
         desc "Add Voltron Encrypt initializer"
 
