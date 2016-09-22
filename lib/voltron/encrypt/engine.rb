e345c49bc39e2b7b216fd587dce2952a8a430097
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 11:33:44 -0600

    Removes isolate_namespace

--- lib/voltron/encrypt/engine.rb
+++ lib/voltron/encrypt/engine.rb
@@ -4,11 +4,10 @@ module Voltron
 	class Encrypt
 		class Engine < Rails::Engine
 
-			isolate_namespace Voltron
-
 			initializer "voltron.encrypt.initialize" do
 				::ActiveRecord::Base.send :extend, ::Voltron::Encryptable
 			end
+
 		end
 	end
 end
